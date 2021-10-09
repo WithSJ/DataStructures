56c6de5a2249959c3ab0a4377c57a2e5111b9e11
Sandeep Jadam <withsj.in@gmail.com>
2021-10-09 23:47:14 +0530

    Array structure init

---
+++ array.py
@@ -0,0 +1,30 @@
+from typing import Mapping
+
+
+class Node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+    
+
+
+class Array:
+    def __init__(self,size):
+        self.arr = Node()
+        temp = self.arr
+        for i in range(size):
+            temp.next = Node()
+            temp.data = i
+            temp = temp.next
+    def show(self,size):
+        temp = self.arr
+        for i in range(size):
+            print(temp.data)
+            temp = temp.next
+
+
+if __name__ == "__main__":
+    arr = Array(10)
+    arr.show(10)
+            
+
