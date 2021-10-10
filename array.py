c9529c9e8fb3813cc10af9b7e7988338d3393c06
Sandeep Jadam <withsj.in@gmail.com>
2021-10-10 18:41:42 +0530

    InsertAt , show, checkIndex, use of [] and repr ,

--- array.py
+++ array.py
@@ -13,18 +13,49 @@ class Array:
         
         for i in range(size):
             temp.next = Node()
-            temp.data = i
             temp = temp.next
     
+    def insertAt(self,index,data):
+        self.checkIndex(index)
+        temp = self.arr
+        for i in range(index):
+            temp = temp.next
+        
+        temp.data = data
+        
+
     def show(self,size):
+        self.checkIndex(size)
         temp = self.arr
         for i in range(size):
-            print(temp.data)
+            print(i," : ",temp.data)
             temp = temp.next
 
+    def checkIndex(self,index):
+        if index > self.size or index < 0:
+            raise IndexError("Out of index")
+    
+    def __repr__(self) -> str:
+        stng = "["
+        temp = self.arr
+        for i in range(self.size):
+            stng += str(temp.data) + ","
+            temp = temp.next
+        return stng[:-1] + "]"
+    
+    def __getitem__(self,index):
+        self.checkIndex(index)
+        temp = self.arr
+        for i in range(index):
+            temp = temp.next
+        
+        return temp.data
 
 if __name__ == "__main__":
     arr = Array(10)
+    arr.insertAt(1,"Data")
     arr.show(10)
+    print(arr[1])
+    print( arr)
             
 
